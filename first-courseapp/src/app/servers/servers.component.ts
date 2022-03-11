445b3825d38f64bdb88a6106e6205c13d09e0cf0
SkyThunder <skysidtim@gmail.com>
2022-03-11 16:36:28 +0100

    playing with some proeprties

--- first-courseapp/src/app/servers/servers.component.ts
+++ first-courseapp/src/app/servers/servers.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = true;
+  serverCreationStatus = "Server not created"
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created"
   }
 
 }
