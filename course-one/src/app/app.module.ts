d4bfb5dbad7b202b356f452f15ecd915a0182b58
SkyThunder <skysidtim@gmail.com>
2022-03-14 15:26:38 +0100

    create components structure

--- course-one/src/app/app.module.ts
+++ course-one/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './features/shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './features/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './features/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './features/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule
