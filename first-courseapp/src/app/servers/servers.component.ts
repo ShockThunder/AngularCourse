4ee7f5a074b5cfb7dfc1948a6110ccaf9bd6456c
SkyThunder <skysidtim@gmail.com>
2022-03-15 17:17:22 +0100

    playing with directives

--- first-courseapp/src/app/servers/servers.component.ts
+++ first-courseapp/src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -18,6 +18,8 @@ export class ServersComponent implements OnInit {
   buttonClicks: string[] = [];
   showDetails = false; 
   username = "";
+
+  @Input() test: string = "no";
   constructor() {}
 
   ngOnInit(): void {
