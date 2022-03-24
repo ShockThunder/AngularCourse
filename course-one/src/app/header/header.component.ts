1fd2a181444e1d128fde6f1a715bd8846f9faa9c
SkyThunder <skysidtim@gmail.com>
2022-03-24 11:40:35 +0100

    add router switch tabs

--- course-one/src/app/header/header.component.ts
+++ course-one/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,18 +7,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() isRecipe = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  openRecipe(){
-    this.isRecipe.emit(true);
-  }
-
-  openShopping(){
-    this.isRecipe.emit(false);
-  }
-
 }
