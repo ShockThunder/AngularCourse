d550c4b6127005e55ef9fc854ce7b160afa0403f
SkyThunder <skysidtim@gmail.com>
2022-03-29 14:11:55 +0200

    play with custom observables

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private firstSub: Subscription;
   constructor() { }
 
-  ngOnInit() {
+  ngOnDestroy(): void {
+    this.firstSub.unsubscribe();
   }
+
+  ngOnInit() {
+    // this.firstSub = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    const customInterval = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000)
+    });
+
+    this.firstSub = customInterval.subscribe(data => {
+      console.log(data);
+    })
+  }
+
 
 }
